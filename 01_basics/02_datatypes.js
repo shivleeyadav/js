fb90cfdbf81e1f978d977350245c579eee5f7811
Shivlee Yadav <shivleeyadav9416@gmail.com>
2024-08-29 17:07:50 +0000

    02done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+ "use strict";  // treat all JS code as newer version
+
+//  alert("Hello")   // we are using nodejs not browser
+
+console.log(3+3);     // code raedability should be high 
+
+console.log("Shivlee");
+
