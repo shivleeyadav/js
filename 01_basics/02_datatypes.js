7192210198b2adf0b39a68e3e303e47562e033c5
Shivlee Yadav <shivleeyadav9416@gmail.com>
2024-08-31 13:37:19 +0000

    02done

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,7 +2,29 @@
 
 //  alert("Hello")   // we are using nodejs not browser
 
-console.log(3+3);     // code raedability should be high 
+console.log(3+3);     // code readability should be high 
 
 console.log("Shivlee");
+
+let name = "Shivlee"
+let age = 18
+let isLoggedIn = false
+let state   //undefined because we defined a variable not its value
+
+
+//number => 2 to power 53
+//bigint 
+//string =>" "
+//boolean => true/false
+//null => stand alone value
+//undefined =>we didnot defined the value
+//symbol => unique
+
+//object 
+
+console.log(typeof "Shivlee");
+console.log(typeof null);   // object
+
+console.log(typeof undefined);  // undefined
+
 
