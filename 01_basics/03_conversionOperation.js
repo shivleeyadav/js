a9ac072c3483683dae5febcb84bfc109dd469b0f
Shivlee Yadav <shivleeyadav9416@gmail.com>
2024-08-31 18:33:38 +0000

    03done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,3 +28,41 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber);
 console.log(stringNumber);
+
+
+//********************* Operations ***************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(typeof negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);   // 2 ki power 3
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = "Shivlee"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1" + 2 + 2);
+
+
+
+
+
+
+
+
+
+
+
+
+
