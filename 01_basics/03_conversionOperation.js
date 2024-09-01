d297635858708f5abf4d8be9e2f21e843805c835
Shivlee Yadav <shivleeyadav9416@gmail.com>
2024-09-01 03:27:48 +0000

    comparisondone

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -53,6 +53,31 @@ console.log(str3);
 console.log("1"+2);
 console.log(1+"2");
 console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(true);    // output is true
+console.log(+true);    // output is one
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ 01_basics/04_comparisson.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);     //true
+console.log("02" > 1);    // true
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null>=0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined<0);
+
+// ===   =>  strictly check the number
+
+console.log("2" === 2);   // This is not correct because data types are different 
+
